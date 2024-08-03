ca17dfde61457894df0d0e154dcf94fb202b78c5
LykourgosS <lpsarantidis@gmail.com>
2024-08-03 12:15:26 +0300

    feat: add simple Product class to use it as data in blockchain

---
+++ src/main/java/com/lykourgoss/example/Product.java
@@ -0,0 +1,65 @@
+package com.lykourgoss.example;
+
+import com.google.gson.Gson;
+import com.lykourgoss.blockchainapi.Blockable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product implements Blockable {
+    private final String code;
+    private String title;
+    private float price;
+    private String description;
+    private String category;
+
+    public Product(Builder builder) {
+        this.code = builder.code;
+        this.title = builder.title;
+        this.price = builder.price;
+        this.description = builder.description;
+        this.category = builder.category;
+    }
+
+    public static final class Builder{
+        private final String code;
+        private String title;
+        private float price;
+        private String description;
+        private String category;
+
+        public Builder(String code) {
+            this.code = code;
+        }
+
+        public Builder title(String title){
+            this.title = title;
+            return this;
+        }
+
+        public Builder price(float price){
+            this.price = price;
+            return this;
+        }
+
+        public Builder description(String description){
+            this.description = description;
+            return this;
+        }
+
+        public Builder category(String category){
+            this.category = category;
+            return this;
+        }
+
+        public Product build(){
+            return new Product(this);
+        }
+    }
+
+    @Override
+    public String getBlockData() {
+        return new Gson().toJson(this);
+    }
+}
