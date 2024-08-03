f74989848b1d412d64136c8d0badfe6b941cfcd4
LykourgosS <lpsarantidis@gmail.com>
2024-08-03 11:33:28 +0300

    feat: create Stringifier class and @Exclude annotation
    @Exclude: is used to exclude fields from object in toString(...) method

---
+++ src/main/java/com/lykourgoss/blockchainapi/helpers/stringifier/Exclude.java
@@ -0,0 +1,11 @@
+package com.lykourgoss.blockchainapi.helpers.stringifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Exclude {
+}
---
+++ src/main/java/com/lykourgoss/blockchainapi/helpers/stringifier/Stringifier.java
@@ -0,0 +1,40 @@
+package com.lykourgoss.blockchainapi.helpers.stringifier;
+
+import java.lang.reflect.Field;
+
+public class Stringifier {
+    public static synchronized String toHexString(byte[] bytes){
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes){
+            builder.append(String.format("%02x",b));
+        }
+        return builder.toString();
+    }
+
+    private static synchronized String toString(Object object, String fieldSeparator){
+        StringBuilder builder = new StringBuilder();
+        for (Field field : object.getClass().getDeclaredFields()){
+            if (field.getAnnotation(Exclude.class) != null){
+                continue;
+            }
+            field.setAccessible(true);
+            String fieldToString;
+            try {
+                if (field.getType().isPrimitive() || field.getType().isAssignableFrom(String.class)){
+                    fieldToString =field.get(object).toString();
+                } else{
+                    fieldToString = Stringifier.toString(field.get(object), fieldSeparator);
+                }
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+            builder.append(fieldToString);
+            builder.append(fieldSeparator);
+        }
+        return builder.toString();
+    }
+
+    public static synchronized String toString(Object object){
+        return toString(object, "");
+    }
+}
