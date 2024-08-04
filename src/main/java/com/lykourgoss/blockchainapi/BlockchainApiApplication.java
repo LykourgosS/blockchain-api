8053aa893fe41fea255713d1a537505c89e4992c
LykourgosS <lpsarantidis@gmail.com>
2024-08-04 19:41:06 +0300

    feat: setup spring boot app to open ControlPanel form

--- src/main/java/com/lykourgoss/blockchainapi/BlockchainApiApplication.java
+++ src/main/java/com/lykourgoss/blockchainapi/BlockchainApiApplication.java
@@ -1,13 +1,28 @@
 package com.lykourgoss.blockchainapi;
 
-import org.springframework.boot.SpringApplication;
+import com.lykourgoss.blockchainapi.gui.ControlPanel;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.swing.*;
+import java.awt.EventQueue;
 
 @SpringBootApplication
 public class BlockchainApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlockchainApiApplication.class, args);
+        var context = new SpringApplicationBuilder(BlockchainApiApplication.class)
+                .headless(false)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        EventQueue.invokeLater(() -> {
+            var form = context.getBean(ControlPanel.class);
+            form.setVisible(true);
+        });
     }
 
 }
