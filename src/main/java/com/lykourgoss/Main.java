343b2d98a625d5c5ea742d38d590ac94e4d57562
LykourgosS <lpsarantidis@gmail.com>
2024-08-04 13:09:34 +0300

    test: Add code in Main, to test functionality of blockchain api so far

--- src/main/java/com/lykourgoss/Main.java
+++ src/main/java/com/lykourgoss/Main.java
@@ -2,23 +2,37 @@ package com.lykourgoss;
 
 import com.google.gson.Gson;
 import com.lykourgoss.blockchainapi.Blockchain;
+import com.lykourgoss.blockchainapi.miners.SingleThreadMiner;
 import com.lykourgoss.example.Product;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Product product = new Product.Builder("123")
                 .title("Test product")
                 .description("Only for testing")
                 .category("dairy")
-                .price(1.05f)
+                .price(1.05F)
                 .build();
 
-        System.out.println(product.getBlockData());
+        Product product2 = new Product.Builder("1234")
+                .title("Test product 2")
+                .description("Only for testing the previous one")
+                .category("not dairy")
+                .price(1)
+                .build();
 
-        Blockchain<Product> chain = new Blockchain<>();
+        Blockchain<Product> chain = new Blockchain<>(new SingleThreadMiner(), 5);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
         chain.addBlock(product);
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
+        chain.addBlock(product2);
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
 
-        System.out.println(new Gson().toJson(chain.getBlocks().getFirst()));
+//        System.out.println(chain.toJson());
+        System.out.println(chain.validate());
     }
 }
