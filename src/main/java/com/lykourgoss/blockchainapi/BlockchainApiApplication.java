eaad24620d2f5fca198ea992b09d48480318512d
LykourgosS <lpsarantidis@gmail.com>
2024-08-04 20:23:55 +0300

    refactor: move blockchain testing code inside BlockchainApiApplication

--- src/main/java/com/lykourgoss/blockchainapi/BlockchainApiApplication.java
+++ src/main/java/com/lykourgoss/blockchainapi/BlockchainApiApplication.java
@@ -1,6 +1,9 @@
 package com.lykourgoss.blockchainapi;
 
+import com.lykourgoss.blockchainapi.core.Blockchain;
 import com.lykourgoss.blockchainapi.gui.ControlPanel;
+import com.lykourgoss.blockchainapi.miners.SingleThreadMiner;
+import com.lykourgoss.example.Product;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +11,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 
 import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 
 @SpringBootApplication
 public class BlockchainApiApplication {
@@ -27,6 +32,34 @@ public class BlockchainApiApplication {
                 ControlPanel form = new ControlPanel();
                 form.setVisible(true);
             });
+            test();
         };
+    }
+
+    private void test() {
+        Product product = new Product.Builder("123")
+                .title("Test product")
+                .description("Only for testing")
+                .category("dairy")
+                .price(1.05F)
+                .build();
+
+        Product product2 = new Product.Builder("1234")
+                .title("Test product 2")
+                .description("Only for testing the previous one")
+                .category("not dairy")
+                .price(1)
+                .build();
+
+        Blockchain<Product> chain = new Blockchain<>(new SingleThreadMiner(), 5);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
+        chain.addBlock(product);
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
+        chain.addBlock(product2);
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
+
+        System.out.println(chain.toJson());
+        System.out.println(chain.validate());
     }
 }
