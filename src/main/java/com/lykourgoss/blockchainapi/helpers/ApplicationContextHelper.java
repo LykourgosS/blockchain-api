480540fc8f35abe41d119552baad6ab9363501e0
LykourgosS <lpsarantidis@gmail.com>
2024-08-10 20:19:11 +0300

    feat: add ApplicationContextHelper to accesss pring boot beans

---
+++ src/main/java/com/lykourgoss/blockchainapi/helpers/ApplicationContextHelper.java
@@ -0,0 +1,27 @@
+package com.lykourgoss.blockchainapi.helpers;
+
+import lombok.Getter;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class ApplicationContextHelper implements ApplicationContextAware {
+
+    @Getter
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        setContext(applicationContext);
+    }
+
+    private static synchronized void setContext(ApplicationContext context) {
+        ApplicationContextHelper.context = context;
+    }
+
+    public static <T> T getBean(Class<T> requiredType){
+        return context.getBean(requiredType);
+    }
+}
