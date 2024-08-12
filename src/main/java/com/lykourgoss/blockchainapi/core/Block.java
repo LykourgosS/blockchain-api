bfcdc060fdf6b7a0a5a0557ba44033f5f51e1ad9
LykourgosS <lpsarantidis@gmail.com>
2024-08-12 14:04:36 +0300

    feat: add @Entity in Block
    (no args constructor is necessary to be able to add @Entity annotation,
    so fields cannot be final)

--- src/main/java/com/lykourgoss/blockchainapi/core/Block.java
+++ src/main/java/com/lykourgoss/blockchainapi/core/Block.java
@@ -3,21 +3,30 @@ package com.lykourgoss.blockchainapi.core;
 import com.lykourgoss.blockchainapi.hashers.SHA256Hasher;
 import com.lykourgoss.blockchainapi.helpers.stringifier.Exclude;
 import com.lykourgoss.blockchainapi.helpers.stringifier.Stringifier;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.Instant;
 
+@Entity
 @Getter
 public class Block<T extends Blockable> {
     @Setter
     @Exclude
+    @Id
     private String hash;
-    private final String previousHash;
-    private final T blockableObject;
-    private final long timestamp;
+    private String previousHash;
+    @Transient
+    private T blockableObject;
+    private long timestamp;
     @Setter
     private int nonce;
+
+    public Block() {
+    }
 
     public Block(String previousHash, T blockableObject) {
         this.hash = "";
@@ -26,7 +35,7 @@ public class Block<T extends Blockable> {
         this.timestamp = Instant.now().toEpochMilli();
     }
 
-    public String stringify(){
+    public String stringify() {
         return Stringifier.toString(this);
     }
 
