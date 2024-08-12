ad5f9b80c7ba7603ef8c7e519cdb0a49abeecb0a
LykourgosS <lpsarantidis@gmail.com>
2024-08-12 14:03:50 +0300

    refactor: delete Blockchain and move functionality to BlockService

--- src/main/java/com/lykourgoss/blockchainapi/persistence/BlockService.java
+++ src/main/java/com/lykourgoss/blockchainapi/persistence/BlockService.java
@@ -2,6 +2,9 @@ package com.lykourgoss.blockchainapi.persistence;
 
 import com.lykourgoss.blockchainapi.core.Block;
 import com.lykourgoss.blockchainapi.core.Blockable;
+import com.lykourgoss.blockchainapi.helpers.jsonizer.GsonJsonizer;
+import com.lykourgoss.blockchainapi.miners.Miner;
+import com.lykourgoss.blockchainapi.validators.Validator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,9 +12,29 @@ import java.util.List;
 @Service
 public class BlockService<T extends Blockable> implements IService<Block<T>, String> {
     private final BlockRepository<T> repository;
+    private final Miner miner;
 
-    public BlockService(final BlockRepository<T> repository) {
+    public BlockService(final BlockRepository<T> repository, final Miner miner) {
         this.repository = repository;
+        this.miner = miner;
+    }
+
+    public boolean validate(){
+        return Validator.INSTANCE.validate(getAll());
+    }
+
+    public String getLastHash(){
+        return getAll().isEmpty() ? "" : getAll().getLast().getHash();
+    }
+
+    public String toJson(){
+        return GsonJsonizer.INSTANCE.toJson(getAll());
+    }
+
+    public Block<T> add(T blockable){
+        Block<T> block = new Block<>(getLastHash(), blockable);
+        miner.mineFor(block);
+        return add(block);
     }
 
     @Override
