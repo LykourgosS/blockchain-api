5817ea35ed178f69104bbbca6f77ea949eb22414
LykourgosS <lpsarantidis@gmail.com>
2024-08-12 17:41:34 +0300

    refactor: move testing code to ControlPanel form
    + use BlockService instead of Blockchain

--- src/main/java/com/lykourgoss/blockchainapi/BlockchainApiApplication.java
+++ src/main/java/com/lykourgoss/blockchainapi/BlockchainApiApplication.java
@@ -1,9 +1,6 @@
 package com.lykourgoss.blockchainapi;
 
-import com.lykourgoss.blockchainapi.core.Blockchain;
 import com.lykourgoss.blockchainapi.gui.ControlPanel;
-import com.lykourgoss.blockchainapi.miners.SingleThreadMiner;
-import com.lykourgoss.example.Product;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +8,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 
 import javax.swing.*;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 
 @SpringBootApplication
 public class BlockchainApiApplication {
@@ -30,36 +25,8 @@ public class BlockchainApiApplication {
             // Ensure Swing components are created on the EDT
             SwingUtilities.invokeLater(() -> {
                 ControlPanel form = new ControlPanel();
-                form.setVisible(true);
+                form.start();
             });
-            test();
         };
-    }
-
-    private void test() {
-        Product product = new Product.Builder("123")
-                .title("Test product")
-                .description("Only for testing")
-                .category("dairy")
-                .price(1.05F)
-                .build();
-
-        Product product2 = new Product.Builder("1234")
-                .title("Test product 2")
-                .description("Only for testing the previous one")
-                .category("not dairy")
-                .price(1)
-                .build();
-
-        Blockchain<Product> chain = new Blockchain<>(new SingleThreadMiner(), 5);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
-        System.out.println(sdf.format(Instant.now().toEpochMilli()));
-        chain.addBlock(product);
-        System.out.println(sdf.format(Instant.now().toEpochMilli()));
-        chain.addBlock(product2);
-        System.out.println(sdf.format(Instant.now().toEpochMilli()));
-
-        System.out.println(chain.toJson());
-        System.out.println(chain.validate());
     }
 }
--- src/main/java/com/lykourgoss/blockchainapi/gui/ControlPanel.java
+++ src/main/java/com/lykourgoss/blockchainapi/gui/ControlPanel.java
@@ -1,13 +1,27 @@
 package com.lykourgoss.blockchainapi.gui;
 
+import com.lykourgoss.blockchainapi.core.Blockable;
+import com.lykourgoss.blockchainapi.helpers.ApplicationContextHelper;
+import com.lykourgoss.blockchainapi.persistence.BlockService;
+import com.lykourgoss.example.Product;
+
 import javax.swing.*;
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 
 public class ControlPanel extends JFrame {
+    private final BlockService<Product> blockService;
+    private final BlockService<Student> blockServiceStudent;
     private JPanel mainPanel;
     private JButton addButton;
 
     public ControlPanel() {
+        this.blockService = ApplicationContextHelper.getBean(BlockService.class);
+        this.blockServiceStudent = ApplicationContextHelper.getBean(BlockService.class);
+    }
+
+    public void start() {
         setTitle("My Form");
         setPreferredSize(new Dimension(420, 650));
         setContentPane(mainPanel);
@@ -15,5 +29,54 @@ public class ControlPanel extends JFrame {
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+
+        test();
+    }
+
+    private void test() {
+        Product product = new Product.Builder("123")
+                .title("Test product")
+                .description("Only for testing")
+                .category("dairy")
+                .price(1.05F)
+                .build();
+
+        Product product2 = new Product.Builder("1234")
+                .title("Test product 2")
+                .description("Only for testing the previous one")
+                .category("not dairy")
+                .price(1)
+                .build();
+
+        var student = new Student("Anna", 24);
+
+        blockService.deleteAll();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
+        blockService.add(product);
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
+        blockService.add(product2);
+        System.out.println(sdf.format(Instant.now().toEpochMilli()));
+        blockServiceStudent.add(student);
+
+        System.out.println(blockService.toJson());
+        System.out.println(blockService.validate());
+    }
+
+    static class Student implements Blockable {
+
+        private final String name;
+        private final int age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String getBlockData() {
+            return name + age;
+        }
     }
 }
