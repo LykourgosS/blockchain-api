65e96a339cb1e58ab92ea8280fdb35340e373027
LykourgosS <lpsarantidis@gmail.com>
2024-08-16 14:21:39 +0300

    feat: add (DataToStringData/StringDataToData)Mapper annotations
    will be used in MapStruct's @Mapping annotation's qualifiedBy property

---
+++ src/main/java/com/lykourgoss/blockchainapi/core/mappers/custom/StringDataToDataMapper.java
@@ -0,0 +1,14 @@
+package com.lykourgoss.blockchainapi.core.mappers.custom;
+
+import org.mapstruct.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Qualifier
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.CLASS)
+public @interface StringDataToDataMapper {
+}
