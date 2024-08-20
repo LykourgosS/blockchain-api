77e2c426d1bdbdca5b3bb9453fd02ed14261bab7
LykourgosS <lpsarantidis@gmail.com>
2024-08-20 21:01:28 +0300

    feat: add MinerBenchmark with singleThreadMining method to benchmark

---
+++ src/main/java/com/lykourgoss/blockchainapi/benchmarking/MinerBenchmark.java
@@ -0,0 +1,23 @@
+package com.lykourgoss.blockchainapi.benchmarking;
+
+import com.lykourgoss.blockchainapi.benchmarking.states.BlockState;
+import com.lykourgoss.blockchainapi.benchmarking.states.SpringBootState;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+public class MinerBenchmark {
+    public static void main(String[] args) throws Exception {
+        new BenchmarkRunner(MinerBenchmark.class).run();
+    }
+
+    @Benchmark
+    public void singleThreadMining(SpringBootState springBootState, BlockState blockState){
+        springBootState.getMiner().mineFor(blockState.getBlock());
+    }
+}
