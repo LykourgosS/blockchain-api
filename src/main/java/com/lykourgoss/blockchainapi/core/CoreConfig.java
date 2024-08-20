8b34103c1a448cd53490be0092b1b51c8aa02821
LykourgosS <lpsarantidis@gmail.com>
2024-08-20 21:06:38 +0300

    fix: add @PropertySource to avoid error resolving @Value for zeros
    (java.lang.IllegalArgumentException: Could not resolve placeholder 'validator.zeros' in value "${validator.zeros}")
    when accessing spring boot application's context outside of application,
    during jmh benchmark (in SpringBootState)
    
    !Note! Another solution could be to set default value for zeros like:
    @Value("${validator.zeros:5}")

--- src/main/java/com/lykourgoss/blockchainapi/core/CoreConfig.java
+++ src/main/java/com/lykourgoss/blockchainapi/core/CoreConfig.java
@@ -1,14 +1,17 @@
 package com.lykourgoss.blockchainapi.core;
 
 import com.lykourgoss.blockchainapi.core.miners.Miner;
+import com.lykourgoss.blockchainapi.core.miners.MultiThreadMiner;
 import com.lykourgoss.blockchainapi.core.miners.SingleThreadMiner;
 import com.lykourgoss.blockchainapi.core.validators.Validator;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class CoreConfig {
     @Value("${validator.zeros}")
     private int VALIDATOR_ZEROS;
