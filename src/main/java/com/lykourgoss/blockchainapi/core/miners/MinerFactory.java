12f9b4b16780feede75b9d9da25f8d03961c1efb
LykourgosS <lpsarantidis@gmail.com>
2024-11-08 17:54:47 +0200

    feat: create MinerFactory to create single or multi thread miners
    + create MinerType enum

---
+++ src/main/java/com/lykourgoss/blockchainapi/core/miners/MinerFactory.java
@@ -0,0 +1,21 @@
+package com.lykourgoss.blockchainapi.core.miners;
+
+import com.lykourgoss.blockchainapi.core.miners.interfaces.Miner;
+
+public enum MinerFactory {
+
+    INSTANCE;
+
+    public Miner getMiner(MinerType minerType, Integer numOfThreads) {
+        return switch (minerType) {
+            case SINGLE_THREAD -> new SingleThreadMiner();
+            case JAVA_API_MULTI_THREAD -> new JavaAPIMultiThreadMiner(numOfThreads);
+            case CUSTOM_MULTI_THREAD -> new CustomMultiThreadMiner(numOfThreads);
+            default -> throw new IllegalArgumentException("Miner type:["+ minerType +"] is unknown.");
+        };
+    }
+
+    public Miner getMiner(MinerType minerType) {
+        return getMiner(minerType, 1);
+    }
+}
---
+++ src/main/java/com/lykourgoss/blockchainapi/core/miners/MinerType.java
@@ -0,0 +1,7 @@
+package com.lykourgoss.blockchainapi.core.miners;
+
+public enum MinerType {
+    SINGLE_THREAD,
+    JAVA_API_MULTI_THREAD,
+    CUSTOM_MULTI_THREAD
+}
