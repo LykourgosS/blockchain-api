4be47d3c70d7c7698193a454936524f612145e01
LykourgosS <lpsarantidis@gmail.com>
2024-11-18 17:48:09 +0200

    refactor: make SingleThreadState independent from SpringBootState
    by using MinerFactory to create SingleThreadMiner object

--- src/main/java/com/lykourgoss/blockchainapi/benchmarking/states/miners/SingleThreadState.java
+++ src/main/java/com/lykourgoss/blockchainapi/benchmarking/states/miners/SingleThreadState.java
@@ -1,17 +1,21 @@
 package com.lykourgoss.blockchainapi.benchmarking.states.miners;
 
-import com.lykourgoss.blockchainapi.benchmarking.states.SpringBootState;
-import com.lykourgoss.blockchainapi.core.miners.SingleThreadMiner;
+import com.lykourgoss.blockchainapi.core.miners.MinerFactory;
+import com.lykourgoss.blockchainapi.core.miners.MinerType;
+import com.lykourgoss.blockchainapi.core.miners.interfaces.Miner;
 import lombok.Getter;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 
 @State(Scope.Benchmark)
 @Getter
 public class SingleThreadState {
-    private SingleThreadMiner singleThreadMiner;
+    private Miner singleThreadMiner;
 
     @Setup(Level.Trial)
-    public void setupMiner(SpringBootState state) {
-        singleThreadMiner = state.getContext().getBean(SingleThreadMiner.class);
+    public void setupMiner() {
+        singleThreadMiner = MinerFactory.INSTANCE.getMiner(MinerType.SINGLE_THREAD);
     }
 }
