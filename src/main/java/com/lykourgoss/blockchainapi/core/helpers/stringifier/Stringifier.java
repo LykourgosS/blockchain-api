7299dc7cea650bd9f2aecb575c50a5b6ff2f1717
LykourgosS <lpsarantidis@gmail.com>
2025-05-20 19:06:44 +0300

    refactor: remove synchronized from Stringifier methods
    - remove unnecessary synchronized keyword for static methods, to avoid
    race conditions in multi-threading mining
    - optimize StringBuilder initialization in toHexString

--- src/main/java/com/lykourgoss/blockchainapi/core/helpers/stringifier/Stringifier.java
+++ src/main/java/com/lykourgoss/blockchainapi/core/helpers/stringifier/Stringifier.java
@@ -3,15 +3,15 @@ package com.lykourgoss.blockchainapi.core.helpers.stringifier;
 import java.lang.reflect.Field;
 
 public final class Stringifier {
-    public static synchronized String toHexString(byte[] bytes){
-        StringBuilder builder = new StringBuilder();
+    public static String toHexString(byte[] bytes){
+        StringBuilder builder = new StringBuilder(bytes.length * 2);
         for (byte b : bytes){
             builder.append(String.format("%02x",b));
         }
         return builder.toString();
     }
 
-    private static synchronized String toString(Object object, String fieldSeparator){
+    private static String toString(Object object, String fieldSeparator){
         StringBuilder builder = new StringBuilder();
         for (Field field : object.getClass().getDeclaredFields()){
             if (field.getAnnotation(Exclude.class) != null){
@@ -34,7 +34,7 @@ public final class Stringifier {
         return builder.toString();
     }
 
-    public static synchronized String toString(Object object){
+    public static String toString(Object object){
         return toString(object, "");
     }
 }
