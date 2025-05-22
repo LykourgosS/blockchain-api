aaba5918ec5c03f8e7f5b2aa889f67d38f1098f5
LykourgosS <lpsarantidis@gmail.com>
2025-05-22 16:45:19 +0300

    chore: adjust logging levels during benchmarking
    - set logging levels for Spring, Hibernate, and Hikari to WARN during
    benchmarking

--- src/main/java/com/lykourgoss/blockchainapi/benchmarking/states/SpringBootState.java
+++ src/main/java/com/lykourgoss/blockchainapi/benchmarking/states/SpringBootState.java
@@ -1,12 +1,13 @@
 package com.lykourgoss.blockchainapi.benchmarking.states;
 
-
+import ch.qos.logback.classic.Logger;
 import com.lykourgoss.blockchainapi.BlockchainApiApplication;
 import com.lykourgoss.blockchainapi.benchmarking.sampler.GenericSampler;
 import com.lykourgoss.blockchainapi.persistence.BlockService;
 import lombok.Getter;
 import lombok.Setter;
 import org.openjdk.jmh.annotations.*;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -20,6 +21,10 @@ public class SpringBootState {
 
     @Setup(Level.Trial)
     public void setupBeans() {
+        ((Logger) LoggerFactory.getLogger("org.springframework")).setLevel(ch.qos.logback.classic.Level.WARN);
+        ((Logger) LoggerFactory.getLogger("org.hibernate")).setLevel(ch.qos.logback.classic.Level.WARN);
+        ((Logger) LoggerFactory.getLogger("com.zaxxer.hikari")).setLevel(ch.qos.logback.classic.Level.WARN);
+
         context = new AnnotationConfigApplicationContext(BlockchainApiApplication.class);
         sampler = context.getBean(GenericSampler.class);
         previousHash = context.getBean(BlockService.class).getLastHash();
