47cc4c3840631bcc9570124e1a84e6dd263c0ac3
LykourgosS <lpsarantidis@gmail.com>
2025-07-03 22:45:18 +0300

    refactor: remove unused generic from BlockchainConfig
    and apply changes to implementations

--- blockchain-api-core/src/main/java/com/lykourgoss/blockchainapi/core/BlockchainConfig.java
+++ blockchain-api-core/src/main/java/com/lykourgoss/blockchainapi/core/BlockchainConfig.java
@@ -2,6 +2,6 @@ package com.lykourgoss.blockchainapi.core;
 
 import com.lykourgoss.blockchainapi.core.helpers.serializer.JsonSerializer;
 
-public interface BlockchainConfig<T> {
+public interface BlockchainConfig {
     JsonSerializer serializer();
 }
--- blockchain-api-example/src/main/java/com/lykourgoss/blockchainapi/example/ExampleConfig.java
+++ blockchain-api-example/src/main/java/com/lykourgoss/blockchainapi/example/ExampleConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ExampleConfig implements BlockchainConfig<Product> {
+public class ExampleConfig implements BlockchainConfig {
     @Bean
     @Override
     public JsonSerializer serializer() {
